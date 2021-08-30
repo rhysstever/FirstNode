df93871abc273e001515c2cc1e4ef489a78a6a47
Rhys Stever <rhys@stever.com>
2021-08-30 13:36:46 -0400

    Added basic web server and nodejs example

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const myData = require('./myData.js');
+const Polygon = require('./Polygon.js');
+const _ = require('underscore');
+const http = require('http');
+
+
+console.log('hello world');
+
+const helloFunction = () => {
+	console.log('hello function');
+}
+
+helloFunction();
+
+myData.getMessage();
+
+const myPolygon = new Polygon(10, 15);
+console.dir(myPolygon.height);
+
+const myArray  = [1, 2, 3, 4, 5];
+const found = _.contains(myArray, 7);
+console.log(found);
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+	if(request.url === '/favicon.ico') {
+		console.log('favicon request');
+	}
+
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	console.log('request received');
+
+	response.write(myData.getMessage());
+	response.end();
+};
+
+http.createServer(onRequest).listen(port);
